b6f66c3ea7f73e3cc4d2b6294a3c59ab63156ee9
Rian Haque <rian.haque@hotmail.co.uk>
2022-10-15 23:51:37 +0100

    Solution

--- median.py
+++ median.py
@@ -7,8 +7,12 @@ while True:
         numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
-        print ("Yo i made some changes wallah")
-        print ("tESTGIT ")
     else:
         break
-print(numbers)
+numbers.sort()
+length = len(numbers)
+if length % 2 == 1:
+    median = numbers[length//2]
+    print (median)
+else:
+    print((numbers[length//2 - 1] + numbers[length//2]) /2 )
